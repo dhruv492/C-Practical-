4b4e856306b5686d1839b165f087f4fb50c26bf8
dhruv492 <dhruvchiragpatel0805@gmail.com>
2024-11-22 13:44:04 +0530

    Update Practical 9.c

--- Practical 9.c
+++ Practical 9.c
@@ -1,1 +1,25 @@
+//In this Practical We are going to Create a grading system using ternary operator..
+#include <stdio.h>
 
+void main()
+{
+    int a;     //a=Marks of Students
+    char *grade;
+
+    printf("Enter the marks of students:");
+    scanf("%d", &a);
+
+    if (a < 0 || a > 100)
+    {
+        printf("Please,Enter The Valid Marks..");
+    }
+    else
+    {
+        *grade = (a >= 90) ? "A" :
+                (a >= 80) ? "B" :
+                (a >= 70) ? "C" :
+                (a >= 60) ? "D" : "F";
+
+        printf("The grade is:%s\n", grade);
+    }
+}
